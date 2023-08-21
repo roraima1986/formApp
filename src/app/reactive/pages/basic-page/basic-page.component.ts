f0b0f9c296a2d650ff86f4e3c7ef7ecbbd18f5b1
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-21 10:19:12 -0400

    Mostrar errores de validacion en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -30,11 +30,14 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb:FormBuilder){}
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    //this.myForm.reset(rtx5090);
   }
 
   onSave():void{
-    if(this.myForm.invalid) return;
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
 
     console.log(this.myForm.value);
 
