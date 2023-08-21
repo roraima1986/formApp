f907cd0b3024ddf28b9e6b0a5e1ac6bdf9807ba3
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-21 11:35:11 -0400

    validaciones Dinamicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -23,6 +23,34 @@ export class DynamicPageComponent {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
+  isValidField(field:string):boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index:number){
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+
+  getFieldError(field:string):string | null{
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracters`;
+      }
+
+    }
+
+    return null;
+  }
+
+
+
   onSubmit():void{
     if(this.myForm.invalid){
       this.myForm.markAllAsTouched();
