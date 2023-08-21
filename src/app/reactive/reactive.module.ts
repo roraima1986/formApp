aa69cbd975c45cff89e6798381728c154d950519
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-21 09:46:01 -0400

    Primeros pasos en formularios reactivos

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,10 +1,14 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Propias
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+
 
 
 @NgModule({
@@ -15,7 +19,8 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
