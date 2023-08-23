3e706d453bcfe569f7df886b89ccdb1409bf3eed
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-23 10:24:31 -0400

    Formulario de Registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+
 
 
 @NgModule({
@@ -11,7 +14,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
